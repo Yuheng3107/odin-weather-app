e5ea64ca0a443eee155634a8270b7ea06bda8486
Kuang Yu Heng <yuheng3107@gmail.com>
2023-06-22 21:28:24 +0800

    Fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,6 @@ function App() {
         backgroundRepeat: "no-repeat",
       }}
     >
-      Test
       <SearchBar
         getWeatherData={getWeatherData}
         parseWeatherData={parseWeatherData}
